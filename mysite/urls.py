e61f26cf2f6a680a5923f87f1264cc1e2b5dc60d
Shokat <shokatsaifi@gmail.com>
2014-08-01 00:36:12 +0530

    Namespacing url names

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 )
