f2be5311aaf7ca9e4ef7e0961c5e81802ea0cd11
Shokat <shokatsaifi@gmail.com>
2014-08-04 23:34:31 +0530

    Incremental commit

--- polls/views.py
+++ polls/views.py
@@ -3,13 +3,11 @@ from django.http import HttpResponse
 from polls.models import Poll
 from django.template import RequestContext, loader
 
+
 def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext (request, {
-		'latest_poll_list' : latest_poll_list,
-	})
-	return HttpResponse(template.render(context))
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll %s." % poll_id)
