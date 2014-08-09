20346eb78f12650b43feb76c6aef049dab6b5675
Shokat <shokatsaifi@gmail.com>
2014-08-10 02:20:12 +0530

    Incremental commit

--- polls/views.py
+++ polls/views.py
@@ -18,19 +18,19 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the poll voting form.
-        return render(request, 'polls/detail.html', {
-            'poll': p,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the poll voting form
+		return render(request, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		seleceted_choice.votes += 1
+		seleceted_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
