527ac1aef8f21df194084cd12f1b7b9e9f7bbc9d
Shokat <shokatsaifi@gmail.com>
2014-08-10 11:41:32 +0530

    Incremental commit

--- polls/views.py
+++ polls/views.py
@@ -12,10 +12,11 @@ def index(request):
 
 def detail(request, poll_id):
 	poll = get_object_or_404(Poll, pk=poll_id)
-	return render(request, 'polls/detail.html', {'poll':poll})
+	return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
@@ -28,8 +29,8 @@ def vote(request, poll_id):
 			'error_message': "You didn't select a choice.",
 			})
 	else:
-		seleceted_choice.votes += 1
-		seleceted_choice.save()
+		selected_choice.votes += 1
+		selected_choice.save()
 		# Always return an HttpResponseRedirect after successfully dealing
 		# with POST data. This prevents data from being posted twice if a
 		# user hits the Back button.
