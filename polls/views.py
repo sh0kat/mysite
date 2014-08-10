a7c3ff9944ffb7ff7ec14c2f6e54cd86c6af5107
Shokat <shokatsaifi@gmail.com>
2014-08-11 00:59:06 +0530

    Incremental commit

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404 
-from django.http import HttpResponse, Http404
-from polls.models import Poll
+from django.http import HttpResponseRedirect, Http404, HttpResponse
+from polls.models import Poll, Choice
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 
 
 def index(request):
