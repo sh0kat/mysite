4e925cc4a2c48a72864f865a968a7acad13ee951
Shokat <shokatsaifi@gmail.com>
2014-08-16 16:37:02 +0530

    Minor changes

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 <h2>Demo app using Django framework !</h2>
-
-http://shrouded-journey-8836.herokuapp.com/
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,18 +11,6 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Quick-start development settings - unsuitable for production
@@ -36,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
